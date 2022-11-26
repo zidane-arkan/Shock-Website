0376d807e9b0e2cff5d8f12922319428d835d4c8
zidane-arkan <zidark897@Gmail.com>
2022-11-26 11:41:36 +0700

    Add discover

--- frontend/shockfilm-web/src/components/page/Home.js
+++ frontend/shockfilm-web/src/components/page/Home.js
@@ -140,7 +140,6 @@ const AboutUs = () => {
                 </div>
             </article>
             <article className="aboutus__box col-sm-12 col-md-12 col-xl-12 position-relative">
-                {/* <img className="turnish__bg__1 position-absolute" src="" alt="turnish__bg__1" /> */}
                 <div className="aboutus__item col-sm-3 col-md-3 col-xl-3">
                     <div className="aboutus__top">
                         <img src={aboutusImg[`${1}`]} alt="Movie-1" width="100%" />
@@ -169,6 +168,116 @@ const AboutUs = () => {
         </section>
     );
 };
+const Discover = () => {
+    const movieImg = {
+        "1": movie_1
+    };
+    return (
+        <section className="row row__margin__bottom row__custom__1">
+            <article className="popular__heading col-md-12 col-xl-12">
+                <h2>Discover</h2>
+                <div className="flex__custom">
+                    <Link className="nav-link nav__link__custom">View all</Link>
+                </div>
+            </article>
+            <section className="row__custom__3">
+                <article className="movie__box col-sm-12 col-md-12 col-xl-12">
+                    <div className="movie__item col-sm-6 col-md-6 col-xl-3">
+                        <div className="videotype__top">
+                            <img src={movieImg[`${1}`]} alt="Movie-1" width="100%" />
+                            <h5>Dua Pilar Satu Atap</h5>
+                            <span className="line"></span>
+                        </div>
+                        <div className="videotype__bottom">
+                            <p>Short Film</p>
+                            <button className="btn btn__see btn-outline-light">See</button>
+                        </div>
+                    </div>
+                    <div className="movie__item col-sm-6 col-md-6 col-xl-3">
+                        <div className="videotype__top">
+                            <img src={movieImg[`${1}`]} alt="Movie-1" width="100%" />
+                            <h5>Dua Pilar Satu Atap</h5>
+                            <span className="line"></span>
+                        </div>
+                        <div className="videotype__bottom">
+                            <p>Short Film</p>
+                            <button className="btn btn__see btn-outline-light">See</button>
+                        </div>
+                    </div>
+                    <div className="movie__item col-sm-6 col-md-6 col-xl-3">
+                        <div className="videotype__top">
+                            <img src={movieImg[`${1}`]} alt="Movie-1" width="100%" />
+                            <h5>Dua Pilar Satu Atap</h5>
+                            <span className="line"></span>
+                        </div>
+                        <div className="videotype__bottom">
+                            <p>Short Film</p>
+                            <button className="btn btn__see btn-outline-light">See</button>
+                        </div>
+                    </div>
+                    <div className="movie__item col-sm-6 col-md-6 col-xl-3">
+                        <div className="videotype__top">
+                            <img src={movieImg[`${1}`]} alt="Movie-1" width="100%" />
+                            <h5>Dua Pilar Satu Atap</h5>
+                            <span className="line"></span>
+                        </div>
+                        <div className="videotype__bottom">
+                            <p>Short Film</p>
+                            <button className="btn btn__see btn-outline-light">See</button>
+                        </div>
+                    </div>
+                </article>
+                <article className="movie__box col-sm-12 col-md-12 col-xl-12 pb-sm-3 pb-xl-5">
+                    <div className="movie__item col-sm-6 col-md-6 col-xl-3">
+                        <div className="videotype__top">
+                            <img src={movieImg[`${1}`]} alt="Movie-1" width="100%" />
+                            <h5>Dua Pilar Satu Atap</h5>
+                            <span className="line"></span>
+                        </div>
+                        <div className="videotype__bottom">
+                            <p>Short Film</p>
+                            <button className="btn btn__see btn-outline-light">See</button>
+                        </div>
+                    </div>
+                    <div className="movie__item col-sm-6 col-md-6 col-xl-3">
+                        <div className="videotype__top">
+                            <img src={movieImg[`${1}`]} alt="Movie-1" width="100%" />
+                            <h5>Dua Pilar Satu Atap</h5>
+                            <span className="line"></span>
+                        </div>
+                        <div className="videotype__bottom">
+                            <p>Short Film</p>
+                            <button className="btn btn__see btn-outline-light">See</button>
+                        </div>
+                    </div>
+                    <div className="movie__item col-sm-6 col-md-6 col-xl-3">
+                        <div className="videotype__top">
+                            <img src={movieImg[`${1}`]} alt="Movie-1" width="100%" />
+                            <h5>Dua Pilar Satu Atap</h5>
+                            <span className="line"></span>
+                        </div>
+                        <div className="videotype__bottom">
+                            <p>Short Film</p>
+                            <button className="btn btn__see btn-outline-light">See</button>
+                        </div>
+                    </div>
+                    <div className="movie__item col-sm-6 col-md-6 col-xl-3">
+                        <div className="videotype__top">
+                            <img src={movieImg[`${1}`]} alt="Movie-1" width="100%" />
+                            <h5>Dua Pilar Satu Atap</h5>
+                            <span className="line"></span>
+                        </div>
+                        <div className="videotype__bottom">
+                            <p>Short Film</p>
+                            <button className="btn btn__see btn-outline-light">See</button>
+                        </div>
+                    </div>
+                </article>
+                <button className="btn__custom btn__contact" type="button">Load More</button>
+            </section>
+        </section>
+    );
+};
 const Home = () => {
     return (
         <motion.div
@@ -183,6 +292,7 @@ const Home = () => {
                 <PopularWorks />
                 <BehindScene />
                 <AboutUs />
+                <Discover />
             </main>
         </motion.div>
     );
