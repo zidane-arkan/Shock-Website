951f057d73048593ebfc4edc6961f4b6520f4c8f
zidane-arkan <zidark897@Gmail.com>
2022-12-13 07:02:46 +0700

    (1360px) Responsive Size V1.0

--- frontend/shockfilm-web/src/App.js
+++ frontend/shockfilm-web/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/layout/Navbar";
 import Footer from './components/layout/Footer';
 import AnimatedRoutes from './components/layout/AnimatedRoutes';
 import './fontawesomeicon';
+import './responsive.css';
 
 function App() {
   return (
