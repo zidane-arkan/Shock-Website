a0b5d4bdf34ce929ada0cfe606947ccd25ea0945
zidane-arkan <zidark897@Gmail.com>
2023-01-06 17:47:25 +0700

    blog mobile responsive

--- frontend/shockfilm-web/src/components/layout/Navbar.js
+++ frontend/shockfilm-web/src/components/layout/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import shockLogo from '../../assets/logo/shockLogo.png';
 import { NavLink, Link } from 'react-router-dom';
 
