e167b8636d31b8683ef59e085399486b88ee3940
zidane-arkan <zidark897@Gmail.com>
2023-01-06 16:47:01 +0700

    Update mobile css and navbar

--- frontend/shockfilm-web/src/components/layout/Navbar.js
+++ frontend/shockfilm-web/src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ import { NavLink,Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="navbar navbar__header navbar-expand-lg navbar-light bg-transparent px-sm-4 py-sm-4 position-absolute ">
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler navbar-toggler__custom" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <Link className="navbar-brand" to="/">
