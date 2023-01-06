7639891620466089567ef1ddfc778cc7f9d13aba
zidane-arkan <zidark897@Gmail.com>
2023-01-06 17:38:44 +0700

    add navbar humberger

--- frontend/shockfilm-web/src/components/layout/Navbar.js
+++ frontend/shockfilm-web/src/components/layout/Navbar.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import shockLogo from '../../assets/logo/shockLogo.png';
-import { NavLink,Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const [isNavResponsive, setNavbarResponsive] = useState(false);
+    const handlerNav = () => {
+        setNavbarResponsive(!isNavResponsive);
+    };
     return (
-        <nav className="navbar navbar__header navbar-expand-lg navbar-light bg-transparent px-sm-4 py-sm-4 position-absolute ">
-            <button className="navbar-toggler navbar-toggler__custom" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <nav className={`${isNavResponsive ? 'bg-black' : 'bg-transparent'} navbar navbar__header navbar-expand-lg navbar-light px-sm-4 py-sm-4 position-absolute`}>
+            <button onClick={handlerNav} className="navbar-toggler navbar-toggler__custom" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <Link className="navbar-brand" to="/">
                 <img src={shockLogo} alt="" />
             </Link>
-            <div className="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo03">
+            <div className={`${isNavResponsive ? 'nav__show' : 'nav__hide'} navbar-collapse justify-content-center `} id="navbarTogglerDemo03">
                 <ul className="navbar-nav navbar__nav__custom mr-auto mt-2 mt-lg-0">
                     <li className="nav-item">
                         <NavLink className={`nav-link`} to="/" activeclassname="active" >Home</NavLink>
